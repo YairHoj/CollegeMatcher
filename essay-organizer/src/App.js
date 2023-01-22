0e47bfffeb19dbcf35712b4cad7b235496f8f82c
jonahzadik <j2zadik2004@gmail.com>
2023-01-22 12:07:38 +0200

    Finished Nav design + added routes

--- essay-organizer/src/App.js
+++ essay-organizer/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 import Nav from "./Components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Nav />
+              </>
+            }
+          />
+          <Route
+            path="/edit"
+            element={
+              <>
+                <h1>Edit</h1>
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
