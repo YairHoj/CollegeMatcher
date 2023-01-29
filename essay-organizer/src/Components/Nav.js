a597d2bd2c82fd1c56f860d88195909ae564e8e2
jonahzadik <j2zadik2004@gmail.com>
2023-01-29 13:27:52 +0200

    burger menu and updated styles

--- essay-organizer/src/Components/Nav.js
+++ essay-organizer/src/Components/Nav.js
@@ -1,22 +1,46 @@
 import React from "react";
 import "../App.css";
 import logo from "../logo.png";
+import burger from "../Hamburger_icon.svg.png";
 
 function Nav() {
+  function handleClick(e) {
+    e.preventDefault();
+    let menu = document.getElementById("menu2");
+    menu.style.display = "flex";
+    menu.style.alignItems = "center";
+    menu.style.justifyContent = "center";
+    let page = document.getElementById("page");
+    page.style.display = "none";
+  }
   return (
-    <div className="flex-container">
-      <div className="logodiv">
-        <img className="logo" src={logo} />
-        <h1 className="logotext">EssayHub</h1>
-      </div>
-      <div className="linkdiv">
-        <h2 className="home">Home</h2>
-        <h2 className="browsecolleges">Browse Colleges</h2>
-        <h2 className="essaymanager">Essay Manager</h2>
-        <div className="buttondiv">
-          <a href="-" className="Login">
-            Log in
+    <div>
+      <div className="flex-container">
+        <a href="/" className="logoroute">
+          <div className="logodiv">
+            <img className="logo" src={logo} />
+            <h1 className="logotext">EssayHub</h1>
+          </div>
+        </a>
+
+        <div className="linkdiv">
+          <a href="/" className="navtext" id="home">
+            Home
           </a>
+          <a href="/browse" className="navtext" id="browsecolleges">
+            Browse Colleges
+          </a>
+          <a href="/essaymanager" className="navtext" id="essaymanager">
+            Essay Manager
+          </a>
+          <div className="buttondiv">
+            <a href="/login" className="Login">
+              Log in
+            </a>
+          </div>
+          <button className="burgerbutton" onClick={handleClick}>
+            <img src={burger} className="burgerimage"></img>
+          </button>
         </div>
       </div>
     </div>
