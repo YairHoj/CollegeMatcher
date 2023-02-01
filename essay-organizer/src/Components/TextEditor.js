9cf0c3d87415bcb96809b775860d54d15b7995b6
tyty-04 <tylerseisner@gmail.com>
2023-02-01 13:15:01 +0200

    a

--- essay-organizer/src/Components/TextEditor.js
+++ essay-organizer/src/Components/TextEditor.js
@@ -1,30 +1,59 @@
 import React from "react";
+import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../Firebase";
 
-function textEditor(props) {
+function TextEditor(props) {
+  const [text, setText] = useState();
   let prompt = props.prompt;
   let countType = props.countType;
-  let count;
-  let school = props.school;
-  if (countType == "Word Count") {
-    count = props.count;
-  } else if (countType == "Character Count") {
-    count = props.count;
-  } else {
-    count = 0;
+  let count = props.count;
+  // let college = props.college;
+
+  async function handleSave() {
+    try {
+      await setDoc(
+        doc(
+          db,
+          JSON.parse(sessionStorage.getItem("user")).email,
+          "College",
+          "Essays",
+          `${prompt}`
+        ),
+        {
+          text,
+          countType,
+          count,
+        }
+      );
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
   return (
     <>
       <h3>{prompt}</h3>
-      <h6>{school}</h6>
+      {/* <h6>{college}</h6> */}
       <textarea
         name={prompt}
         rows={30}
         cols={50}
         value={text}
+        onChange={handleChange}
       ></textarea>
-      <p>{count}</p>
+      <p>
+        {count} {countType}
+      </p>
+      <button id="save" onClick={handleSave}>
+        Save
+      </button>
     </>
   );
 }
 
-export default textEditor;
+export default TextEditor;
