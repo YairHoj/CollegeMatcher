3e75b60ed68cc9e4388c43fc9875241d59b5a87b
jonahzadik <j2zadik2004@gmail.com>
2023-02-09 12:57:04 +0200

    More css on essaymanager pt3

--- essay-organizer/src/Components/TextEditor.js
+++ essay-organizer/src/Components/TextEditor.js
@@ -106,21 +106,23 @@ function TextEditor(props) {
       <p id="wordcount" style={{ color: color }}>
         {currCount} / {count} {countType}{" "}
       </p>
-      <CopyToClipboard
-        text={text}
-        onCopy={() =>
-          NotificationManager.success(
-            "Successfully copied to clipboard.",
-            "Success!",
-            3000
-          )
-        }
-      >
-        <button>Copy to clipboard</button>
-      </CopyToClipboard>
-      <button id="save" onClick={handleSave}>
-        Save
-      </button>
+      <div id="essaybuttons">
+        <button id="save" onClick={handleSave}>
+          Save
+        </button>
+        <CopyToClipboard
+          text={text}
+          onCopy={() =>
+            NotificationManager.success(
+              "Successfully copied to clipboard.",
+              "Success!",
+              3000
+            )
+          }
+        >
+          <button id="save">Copy to clipboard</button>
+        </CopyToClipboard>
+      </div>
     </>
   );
 }
